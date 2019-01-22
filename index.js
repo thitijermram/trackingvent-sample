0172f42305695f3f6a7429f7f8a1eed80b8a4f6a
thitijermram <36046946+thitijermram@users.noreply.github.com>
2019-01-22 17:35:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const bodyPaser = require('body-parser');
 const mysql = require('mysql');
-
 const app = express();
 
 app.use(
@@ -15,44 +14,29 @@ app.use(
 app.use(bodyPaser.json());
 
 app.post('/monitor - custom - yes',(req,res) => {
-    /*
-    var user = req.body.result.parameters.username
-        ? req.body.result.parameters.echoText
-        : "Seems like some problem. Speak again.";
-    
-    var pass = req.body.result.parameters.password
-        ? req.body.result.parameters.echoText
-        : "Seems like some problem. Speak again.";
-    
-    */
-    
-    var speech = 'hellofromtheotherside';
-    
-    /*
+    var user = req.body.result.parameters.username;
+    var pass = req.body.result.parameters.password;
+
+    const db = mysql.createConnection({
+            host: 'localhost',
+            user: 'id7769008_thiti7600',
+            pass: 'dew28433',
+            db  : 'id7769008_trackingventilator'
+    });
+
     let sql = `SELECT * FROM customer WHERE Cus_ID = ${user} and Cus_Pass ${pass}`;
-    let query = db.query(sql, (err,result) => {
-        if(err) speech = 'Cannot Found';
-        speech = 'Found!';
-    });
-    */
+
+    let speech = 'hellofromtheotherside';
     
-    return res.json({
-            speech: speech,
-            displayText: speech,
-            source: "webhook-echo-sample"
+    db.query(sql, (err,result) => {
+        if(err) console.log('Cannot Query');
+        speech = result;
     });
 
-});
-
-app.post("/echo", function(req, res) {
-  var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
+    return res.json({
+            "type": "text",
+            "text": speech
+    });
 });
 
 app.listen(process.env.PORT ||'3000',() => {
