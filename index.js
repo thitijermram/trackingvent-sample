d5de59f6a0eb71820f4ebeb8a0f84754e3559ae5
thitijermram <36046946+thitijermram@users.noreply.github.com>
2019-01-22 16:57:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ const express = require('express');
 const bodyPaser = require('body-parser');
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    host     :  'localhost',
-    user     :  'id7769008_thiti7600',
-    password :  'dew28433',
-    database :  'id7769008_trackingventilator'
-});
-
-db.connect((err) => {
-    if(err){
-        console.log(err);
-    }
-    console.log('MySql Connected...');
-});
-
 const app = express();
 
 app.use(
@@ -29,18 +15,7 @@ app.use(
 app.use(bodyPaser.json());
 
 app.post('/monitor-custom-yes',(req,res) => {
-    //var user = req.body.result.parameters.username;
-    //var pass = req.body.result.parameters.password;
-    //var speech = req.body.result.parameters.username: "Seems like some problem. Speak again.";
-    //let sql = `SELECT * FROM customer WHERE Cus_ID = ${user} and Cus_Pass ${pass}`;
-    //let query = db.query(sql, (err,result) => {
-    //    if(err) speech = 'Cannot Found';
-    //    speech = 'Found!';
-    //});
-    var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
     return res.json({
@@ -48,17 +23,11 @@ app.post('/monitor-custom-yes',(req,res) => {
     displayText: speech,
     source: "webhook-echo-sample"
     });
-    //return res.json({
-    //   "type": "text",
-    //   "text": speech
-    //});
+
 });
 
 app.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
+  var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
