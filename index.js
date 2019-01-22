5cab5151cb58ebbe05fba2b543a3cd9e70a316b2
thitijermram <36046946+thitijermram@users.noreply.github.com>
2019-01-22 18:01:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,17 +17,14 @@ app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
-app.post('/monitor - custom - yes',(req,res) => {
-    var user = req.body.result &&
-                req.body.result.parameters &&
-                req.body.result.parameters.username
-                ? req.body.result.parameters.username
-                : "Seems like some problem. Speak again.";
-    var pass = req.body.result &&
-                req.body.result.parameters &&
-                req.body.result.parameters.password
-                ? req.body.result.parameters.password
-                : "Seems like some problem. Speak again.";
+app.post('/monitor -custom - yes',(req,res) => {
+    var user =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.username
+      ? req.body.result.parameters.username
+      : "Seems like some problem. Speak again.";
+    
     return res.json({
             speech: user,
             displayText: user,
