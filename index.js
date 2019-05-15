647397022c54f5c806257f4266acc7fa9ac8b26a
thitijermram <36046946+thitijermram@users.noreply.github.com>
2019-05-15 21:22:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
-const functions = require("firebase-functions");
-const request = require("request-promise");
+"use strict";
 
-exports.LineAdapter = functions.https.onRequest((req, res) => {
-  if (req.method === "POST") {
-    if (req.body.events[0].message.type !== "text") {
-      reply(req);
-    } else {
-      postToDialogflow(req);
-    }
-  }
-  return res.status(200).send(req.method);
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/echo", function(req, res) {
+  var speech = "come on";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
 });
 
-const reply = req => {
-  return request({
-    method: "POST",
-    uri: `${LINE_MESSAGING_API}/reply`,
-    headers: LINE_HEADER,
-    body: JSON.stringify({
-      replyToken: req.body.events[0].replyToken,
-      messages: [
-        {
-          type: "text",
-          text: JSON.stringify(req.body)
-        }
-      ]
-    })
-  });
-};
-
-const postToDialogflow = req => {
-  req.headers.host = "bots.dialogflow.com";
-  return request({
-    method: "POST",
-    uri: "https://bots.dialogflow.com/line/85659568-b9dd-48b8-ab2e-1b47ca5706ab/webhook",
-    headers: req.headers,
-    body: JSON.stringify(req.body)
-  });
-};
+restService.listen(process.env.PORT || 8000, function() {
+    console.log("Server up and listening");
+});
